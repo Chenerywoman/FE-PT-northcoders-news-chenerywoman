82c0535df4ebe47b627d469d190ae099010f0e95
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-29 13:38:32 +0100

    adding handleUpClick & handleDownClick methods to increase & decrease article votes

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {changeArticleVote} from '../api';
 
-const Votes = () => {
+class Votes extends Component {
 
-    return (
-        <div>
-            <h2>Votes</h2>
-            <button>up</button>
-            <button>down</button>
-        </div>
+    state = {
+        votes: this.props.votes,
+        articleId: this.props.articleId
+    }
 
-    )
+    componentDidMount(){
+
+// do I need this?
+    }
+
+    handleUpClick = () => {
+        console.log('votes before', this.state.votes)
+        this.setState({votes: this.state.votes + 1})
+        // why is this the same as before 
+        console.log('votes after', this.state.votes)
+        return changeArticleVote('up', this.state.articleId)
+        .catch(err => console.log(err))
+    }
+
+    handleDownClick = () => {
+        console.log('in handleDownclick')
+        this.setState({votes: this.state.votes - 1})
+        return changeArticleVote('down', this.state.articleId)
+        .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Votes</h4>
+                <p>votes: {this.state.votes}</p>
+                <button onClick={this.handleUpClick} >Up Vote</button>
+                <button onClick={this.handleDownClick} >Down Vote</button>
+            </div>
+
+        )
+    }
 }
 
 export default Votes;
