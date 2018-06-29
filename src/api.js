c8c9c58fd49c39e724b58985ed85be06057451f8
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-29 13:37:02 +0100

    adding changeArticleVote function to file

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 // put fetches in here
 
 export const fetchAllArticles = () => {
-    return fetch(API_URL)
+    return fetch(`${API_URL}/articles`)
     .then(res => {
         if (res.status === 404) throw new Error(res.statusText)
         return res.json()
@@ -11,3 +11,14 @@ export const fetchAllArticles = () => {
     .then(({articles}) => articles)
 }
 
+export const changeArticleVote = (vote, id) => {
+    const url = `${API_URL}/articles/${id}?vote=${vote}`
+    return fetch(url, {
+        method: 'PUT'
+    })
+    .then(res => {
+        if (res.status === 400 || res.status === 404) throw new Error(res.statusText)
+        return res.json();
+    })
+    .then(res => console.log(res))
+}
