8b92a0b14dbf209932ea4aaaf0cdde747513f74b
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-30 22:04:01 +0100

    creating page to render a single article component & related components

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+import Article from '../components/Article';
+import { fetchArticleById } from '../api'
+
+class ArticlePage extends Component {
+
+    state = {
+        article: {},
+        loading: true
+    }
+
+    fetchArticle = (id) => {
+        return fetchArticleById(id)
+            .then(article => {
+                this.setState({ article, loading: false })
+            })
+            .catch(error => this.props.history.push('/404'))
+
+    }
+
+    componentDidMount() {
+        const articleId = this.props.match.params.id;
+        this.fetchArticle(articleId)
+    }
+
+    render() {
+        const { loading, article } = this.state
+        return (<div>
+            {loading ? <p>Loading...</p> :
+                <Article key={article._id} article={article} />
+            }
+            <p>CommentBox</p>
+            <p>commentsList</p>
+        </div>
+        )
+    }
+}
+
+ArticlePage.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            id: PropTypes.string.isRequired
+        })
+    }),
+
+    history: PropTypes.object.isRequired
+}
+
+export default ArticlePage;
