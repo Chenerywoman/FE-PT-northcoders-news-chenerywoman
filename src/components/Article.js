dd5ecb89f7ec1bb7c08e3812dfc1d5cf31c2c056
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-30 22:01:00 +0100

    refactorng to destructure article

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,13 +5,13 @@ const Article = ({article}) => {
 
     return (
         <div>
-            <h4>{article.title}</h4> 
+            <p>{article._id}</p>
+             <h4>{article.title}</h4> 
             <div>topic:{article.belongs_to.title}</div>
-          <p>by:{article.created_by.username}</p>
-          <p>when created - author attribution???</p>
+     <p>created by: {article.created_by.username}</p>
           <p>{article.body}</p>
           <p>comments: {article.comments}</p>
-          <Votes votes={article.votes} id={article._id} route="articles"/>
+          <Votes votes={article.votes} id={article._id} route="articles"/> 
         </div>
 
     )
