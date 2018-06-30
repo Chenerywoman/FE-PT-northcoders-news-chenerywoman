8db9db27c10c2d0e0dd61e0ef6d0cc5010f3a318
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-30 14:51:56 +0100

    removing console.log

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Article from './Article';
 
 const ArticleList = ({articles, topic}) => {
-console.log('topic', topic)
     return (
         <div>
             <h2>Articles {topic ? <span> about {topic} </span> : '' } </h2>
