b1c38fee42bc2fb0812ec89be4bbff5b6a6fa70b
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-30 12:24:06 +0100

    rendering additional properties of article

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,9 +5,12 @@ const Article = ({article}) => {
 
     return (
         <div>
-            <h4>{article.title}</h4>
-          <div>picture</div>
-          <div>{article.body}</div>
+            <h4>{article.title}</h4> 
+            <div>topic:{article.belongs_to.title}</div>
+          <p>by:{article.created_by.username}</p>
+          <p>when created - author attribution???</p>
+          <p>{article.body}</p>
+          <p>comments: {article.comments}</p>
           <Votes votes={article.votes} id={article._id} route="articles"/>
         </div>
 
