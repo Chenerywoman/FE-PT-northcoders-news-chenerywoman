737d590e97d26d0841a2abb9e9502a6b0bfee23a
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 17:00:00 +0100

    adding NoMatch to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import HomePage from './HomePage';
 import ArticlesPage from './ArticlesPage';
 import ArticlePage from './ArticlePage';
+import NoMatchPage from './NoMatchPage';
 
-export {HomePage, ArticlesPage, ArticlePage};
+export {HomePage, ArticlesPage, ArticlePage, NoMatchPage};
