b3dabce58b235a857f5c114a82a0c1a4f310034f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 16:33:21 +0100

    adding error handling so only comment creator can delete it

--- src/components/DeleteComment.js
+++ src/components/DeleteComment.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class DeleteComment extends Component  {
 
     state = {
-        userName: ''
+        userName: '',
+        err: ''
     }
 
     handleNameChange = event => {
@@ -12,17 +13,21 @@ class DeleteComment extends Component  {
 
 handleClick = (event) => {
     event.preventDefault();
+    if (this.props.name !== this.state.userName) { this.setState({err: 'only comment creator can delete comment'}) }
+    else {
     this.props.deleteComment(this.props.id, this.state.userName)
     this.setState({userName: ''})
+    }
 }
 
     render() {
         return <form onSubmit={this.handleClick}>
         <h4>Delete comment: comment creator only</h4>
+        {this.state.err ? <div> {this.state.err}</div> : <div></div> }
         <label> 
         username: <input name='userName' type='text' placeholder='username here' onChange={this.handleNameChange} value={this.state.userName} /> 
         </label>
-        <button type="submit">Delete</button>
+        <button type="submit" >Delete</button>
         </form>
     }
     
