dcc5999fec60777f078c62414550339bc5ce22c9
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 19:47:20 +0100

    mostPopular, mostVotes & mostRecent

--- src/dataFunctions/helpers.js
+++ src/dataFunctions/helpers.js
@@ -1,15 +1,23 @@
 
 export const mostPopular = (articles) => {
 
-    return articles.sort(article(a, b) => {
-
-    const aValue = a.votes + a.comments;
-    const bValue = b.votes + b.comments;
-    if (aValue < bValue) return -1;
-    else if (bValue > aValue) return 1;
-    else return 0;
+    return articles.sort((a, b) => {
+     return (b.votes + b.comments) - (a.votes + a.comments)
 
 })
 
+}
+export const mostVoted = (comments) => {
 
+    return comments.sort((a, b) => {
+     return (b.votes) - (a.votes)
+
+})
+}
+export const mostRecent = (comments) => {
+
+    return comments.sort((a, b) => {
+     return (b.created_at) - (a.created_at)
+
+})
 }
