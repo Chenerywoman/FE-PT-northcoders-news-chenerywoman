6d0b342107b917a76fa5cdc2e14e5c74062113ce
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 11:14:14 +0100

    adding DeleteComment & correcting name of props passed to User

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import User from './User';
 import Votes from './Votes';
+import DeleteComment from './DeleteComment';
 
 const Comment = ({ comment }) => {
-
+// console.log('comment', comment.created_by.username)
     return (
         <div>
             <p> date:{comment.created_at} </p>
             <p>{comment.body}</p>
             <Votes votes={comment.votes} id={comment._id} route='comments' />
-            <User username={comment.created_by.username} avatar={comment.created_by.avatar_url} />
+            <User name={comment.created_by.username} avatar={comment.created_by.avatar_url} />
+            <DeleteComment id={comment._id}/>
         </div>
     )
 }
