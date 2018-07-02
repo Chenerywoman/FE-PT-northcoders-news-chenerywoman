c1e7c63bbc41b9cf516cb879138f40c6a80847fb
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 11:16:16 +0100

    making into function as class not needed

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Comment from './Comment';
 
-const CommentsList = ({comments}) => {
-return (  
-<div>
-<h4>Comments</h4>
-{comments.map(comment => <Comment key={comment._id} comment={comment}/> )} 
-</div>
-)
+const CommentsList = ({comments}) =>  {
+
+        return (
+            <div>
+                <h4>Comments</h4>
+                {comments.map(comment => <Comment key={comment._id} comment={comment} />)}
+            </div>
+        )
 }
 
 CommentsList.propTypes = {
