cabdfd16b593ed48adbca5f9f04fef5ced32cc39
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 11:21:24 +0100

    adding postComment function & passing to InputBox

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import Article from '../components/Article';
 import InputBox from '../components/InputBox';
 import CommentsList from '../components/CommentsList';
 
-import { fetchArticleById, fetchCommentsForArticle } from '../api'
+import { fetchArticleById, fetchCommentsForArticle, postText } from '../api'
 
 class ArticlePage extends Component {
 
@@ -30,9 +30,15 @@ class ArticlePage extends Component {
             this.setState({comments, loading: false})
         })
         .catch(error => this.props.history.push('/404'))
-
     }
 
+    postComment = (created_by, comment ) => {
+       return  postText(created_by, comment, 'articles', this.props.match.params.id, 'comments' )
+        .then(res => { 
+            const amendedComments = [res.new_comment, ...this.state.comments]
+            this.setState({comments: amendedComments})
+        })
+    }
 
     componentDidMount() {
         const articleId = this.props.match.params.id;
@@ -40,13 +46,14 @@ class ArticlePage extends Component {
         .then(() => this.fetchComments(articleId));
     }
 
+
     render() {
         const { loading, article } = this.state
         return (<div>
             {loading ? <p>Loading...</p> :
-                <Article key={article._id} article={article} />
+            <Article key={article._id} article={article} />
             }
-            <InputBox route='articles' id={this.props.match.params.id} endpoint='comments' />
+            <InputBox  postComment={this.postComment}/>
             {loading ? <p>Loading...</p> :
             <CommentsList  comments={this.state.comments}/>
             }
