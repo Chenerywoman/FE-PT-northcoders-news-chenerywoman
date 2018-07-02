4ec6e87175aee8d814fea5ec70d096ee5cb7667c
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-02 19:18:17 +0100

    adding 'created by'

--- src/components/Article.js
+++ src/components/Article.js
@@ -11,7 +11,7 @@ const Article = ({article}) => {
         <div>
            <Link key={article._id} to={`/articles/${article._id}`}>  <p>{article._id}</p> < /Link>
              <h4>{article.title}</h4> 
-             <User name={article.created_by.username} avatar={article.created_by.avatar_url} />
+           <div>  created by: </div> <User name={article.created_by.username} avatar={article.created_by.avatar_url} /> 
             <div>topic:{article.belongs_to.title}</div>
           <p>{article.body}</p>
           <p>comments: {article.comments}</p>
