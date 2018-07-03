08100c4a9fc90e6e1b722266e36ac48ba5e1d9d0
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-03 21:42:52 +0100

    adding link to articles

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Homepage extends Component {
 
 render(){
    return ( 
+    <div>
    <div>Here's the Homepage</div>
+   <Link to={`/articles`} > <p>articles</p> </Link> 
+   </div>
 )
 }
 
