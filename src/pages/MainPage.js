739c215a6e70b9308577931cff2da6121f404454
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-03 22:16:34 +0100

    removing console.log

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import {ArticleList, Navbar} from '../components'
-import {fetchAllArticles} from '../dataFunctions/api';
+import { ArticleList, Navbar } from '../components'
+import { fetchAllArticles } from '../dataFunctions/api';
 
 class MainPage extends Component {
 
@@ -11,7 +11,7 @@ class MainPage extends Component {
     loading: true,
     page: this.props.location.search ? /\d/.exec(this.props.location.search) : 1,
     index: this.props.location.search ? /\d/.exec(this.props.location.search) - 1 : 0,
- }
+  }
 
   fetchArticles = () => {
     return fetchAllArticles()
@@ -28,43 +28,42 @@ class MainPage extends Component {
       pathname: '/articles',
       search: `?page=${this.state.page + 1}`
     })
-    this.setState({index: this.state.index + 1, page: this.state.page + 1})
-}
+    this.setState({ index: this.state.index + 1, page: this.state.page + 1 })
+  }
 
-handleDownClick = (event) => {
-  this.props.history.push({
-    pathname: '/articles',
-    search: `?page=${this.state.page - 1}`
-  })
-  this.setState({index: this.state.index - 1, page: this.state.page - 1})
-}
+  handleDownClick = (event) => {
+    this.props.history.push({
+      pathname: '/articles',
+      search: `?page=${this.state.page - 1}`
+    })
+    this.setState({ index: this.state.index - 1, page: this.state.page - 1 })
+  }
 
   componentDidMount() {
     this.fetchArticles()
   }
 
   render() {
-    console.log('page', this.state.page, 'length', this.state.articles.length)
-    return (  
-     <div>
+    return (
+      <div>
 
-           <Navbar />
-          {this.state.loading ? <div>Loading...</div>
-            :
-            <div>
-              <button onClick={this.handleDownClick} disabled={ this.state.page < 2 ? true : false}> Down one page...</button> 
-             <button onClick={this.handleUpClick}  disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button> 
-             <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
+        <Navbar />
+        {this.state.loading ? <div>Loading...</div>
+          :
+          <div>
+            <button onClick={this.handleDownClick} disabled={this.state.page < 2 ? true : false}> Down one page...</button>
+            <button onClick={this.handleUpClick} disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button>
+            <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
             <ArticleList topic='' articles={this.state.articles[this.state.index]} />
-            </div>
-          }
+          </div>
+        }
       </div>
     );
 
   }
 }
 
-MainPage.propTypes =  {
+MainPage.propTypes = {
   history: PropTypes.object.isRequired
 }
 
