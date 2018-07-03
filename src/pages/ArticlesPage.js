c9257bf4bb2dd347f3211787c04d1f5b01b75c19
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-03 22:12:41 +0100

    adding pagination to ArticlesPage.js

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import ArticleList from '../components/ArticleList'
-import {fetchTopicArticles} from '../dataFunctions/api';
+import { ArticleList, Navbar } from '../components'
+
+
+import { fetchTopicArticles } from '../dataFunctions/api';
 
 class ArticlesPage extends Component {
 
     state = {
         articles: [],
         loading: true,
+        page: this.props.location.search ? /\d/.exec(this.props.location.search) : 1,
+        index: this.props.location.search ? /\d/.exec(this.props.location.search) - 1 : 0,
     }
 
     fetchArticlesByTopic = (topic) => {
-       
+
         return fetchTopicArticles(topic)
             .then(articles => {
                 console.log(articles)
-                this.setState({articles, loading: false })
+                this.setState({ articles, loading: false })
             })
             .catch(error => {
                 this.props.history.push('/404');
             });
+    }
+
+    handleUpClick = (event) => {
+        this.props.history.push({
+            pathname: `/articles/${this.props.match.params.topic}`,
+            search: `?page=${this.state.page + 1}`
+        })
+        this.setState({ index: this.state.index + 1, page: this.state.page + 1 })
+    }
+
+    handleDownClick = (event) => {
+        this.props.history.push({
+            pathname: `/articles/${this.props.match.params.topic}`,
+            search: `?page=${this.state.page - 1}`
+        })
+        this.setState({ index: this.state.index - 1, page: this.state.page - 1 })
     }
 
     componentDidMount() {
@@ -29,24 +49,28 @@ class ArticlesPage extends Component {
     }
 
     componentDidUpdate(prevProps) {
-       
+
         const currTopic = this.props.match.params.topic
         const prevTopic = prevProps.match.params.topic
 
         if (currTopic !== prevTopic) {
-            this.setState({loading: true})
+            this.setState({ loading: true })
             this.fetchArticlesByTopic(currTopic)
         }
     }
 
     render() {
-        const queryString =  /\d/.exec(this.props.location.search);
-        const page = this.props.location.search ? queryString[0] - 1: 0;
         return (
             <div>
+                <Navbar />
                 {this.state.loading ? <div>Loading...</div>
                     :
-                    <ArticleList topic={this.props.match.params.topic} articles={this.state.articles[page]} />
+                    <div>
+                        <button onClick={this.handleDownClick} disabled={this.state.page < 2 ? true : false}> Down one page...</button>
+                        <button onClick={this.handleUpClick} disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button>
+                        <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
+                        <ArticleList topic={this.props.match.params.topic} articles={this.state.articles[this.state.index]} />
+                    </div>
                 }
             </div>
         );
