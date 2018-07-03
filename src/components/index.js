2ba190194ccbb2db15b45ae467800929f2139bd8
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-03 21:40:38 +0100

    adding index file to components folder

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Article from './Article';
+import ArticleList from './ArticleList';
+import Comment from './Comment';
+import CommentsList from './CommentsList';
+import InputBox from './InputBox';
+import Navbar from './Navbar';
+import TopicChooser from './TopicChooser';
+import User from './User';
+import Votes from './Votes';
+
+export {Article, ArticleList, Comment, CommentsList, InputBox, Navbar, TopicChooser, User, Votes}
