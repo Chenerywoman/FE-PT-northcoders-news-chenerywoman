3f3af8f139596725d9934a624ddb08ddff2a7bec
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-03 21:42:16 +0100

    adding buttons to put pages up & down plus displaying query string in the url display

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,45 +1,70 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import ArticleList from '../components/ArticleList'
+import {ArticleList, Navbar} from '../components'
 import {fetchAllArticles} from '../dataFunctions/api';
 
 class MainPage extends Component {
 
   state = {
     articles: [],
-    loading: true, 
-  }
+    loading: true,
+    page: this.props.location.search ? /\d/.exec(this.props.location.search) : 1,
+    index: this.props.location.search ? /\d/.exec(this.props.location.search) - 1 : 0,
+ }
 
   fetchArticles = () => {
     return fetchAllArticles()
       .then(articles => {
-        this.setState({ articles: articles, loading: false })
+        this.setState({ articles, loading: false })
       })
       .catch(error => {
         this.props.history.push('/404');
       });
   };
 
+  handleUpClick = (event) => {
+    this.props.history.push({
+      pathname: '/articles',
+      search: `?page=${this.state.page + 1}`
+    })
+    this.setState({index: this.state.index + 1, page: this.state.page + 1})
+}
+
+handleDownClick = (event) => {
+  this.props.history.push({
+    pathname: '/articles',
+    search: `?page=${this.state.page - 1}`
+  })
+  this.setState({index: this.state.index - 1, page: this.state.page - 1})
+}
+
   componentDidMount() {
     this.fetchArticles()
   }
 
   render() {
-    const queryString =  /\d/.exec(this.props.location.search)[0] - 1
-    const page = this.props.location.search ? queryString : 0
-    return (
-      <div>
+    console.log('page', this.state.page, 'length', this.state.articles.length)
+    return (  
+     <div>
+
+           <Navbar />
           {this.state.loading ? <div>Loading...</div>
             :
-            <ArticleList topic='' articles={this.state.articles[page]} />
+            <div>
+              <button onClick={this.handleDownClick} disabled={ this.state.page < 2 ? true : false}> Down one page...</button> 
+             <button onClick={this.handleUpClick}  disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button> 
+             <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
+            <ArticleList topic='' articles={this.state.articles[this.state.index]} />
+            </div>
           }
       </div>
     );
+
   }
 }
 
-MainPage.propTypes = {
+MainPage.propTypes =  {
   history: PropTypes.object.isRequired
 }
 
