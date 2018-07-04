d43eb3c3105a58a15bc501fd87fb6666bbaeda76
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 14:07:24 +0100

    receiving topic as props & passing to TopicChooser

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import TopicChooser from './TopicChooser';
 
-const Navbar = ({username}) => (<nav> 
+const Navbar = ({username, topic}) => (<nav> 
     <h2>Logged in as {username}</h2> 
-    <TopicChooser />
+    <TopicChooser topic={topic}/>
 </nav>)
 
 export default Navbar;
