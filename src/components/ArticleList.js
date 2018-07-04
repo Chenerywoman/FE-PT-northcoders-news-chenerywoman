69d9fcc4f88295d5d33abe1d40a44664dad73916
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 11:59:43 +0100

    adding ternary to check for undefined articles

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 
 import Article from './Article';
 
-const ArticleList = ({articles, topic}) => {
+const ArticleList = ({articles, topic, }) => {
     return (
+        !articles ? <Redirect to='/404' /> :
         <div>
             <h2>Articles {topic ? <span> about {topic} </span> : '' } </h2>
             {articles.map(article => 
