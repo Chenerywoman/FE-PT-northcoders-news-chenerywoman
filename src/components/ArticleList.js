ec2a3c4c7af9474c2538c8861aea986f68b09af9
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 20:25:38 +0100

    removing username from props

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,13 +3,13 @@ import {Redirect} from 'react-router-dom';
 
 import Article from './Article';
 
-const ArticleList = ({articles, topic, }) => {
+const ArticleList = ({articles, topic}) => {
     return (
         !articles ? <Redirect to='/404' /> :
         <div>
             <h2>Articles {topic ? <span> about {topic} </span> : '' } </h2>
             {articles.map(article => 
-             <Article key={article._id} article={article}/>
+             <Article key={article._id} article={article} />
             )}
         </div>
 
