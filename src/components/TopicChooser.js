6012258d9fa0253ba557960c03a07ed395685328
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 14:08:07 +0100

    adding ternary statements to only show buttons for other topics

--- src/components/TopicChooser.js
+++ src/components/TopicChooser.js
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom';
 class TopicChooser extends Component {
 
     render () {
+        console.log('this.props.topic', this.props.topic)
     return (
         <React.Fragment>
-       <p> Choose a topic:</p>
-            <Link to='/articles/topic/coding' > <button>coding</button> </Link>
-            <Link to='/articles/topic/football' > <button>football</button> </Link>
-            <Link to='/articles/topic/cooking' > <button>cooking</button> </Link>
+       <p> Choose another topic:</p> 
+           {this.props.topic !== 'coding' ? <Link to='/articles/topic/coding' > <button>coding</button> </Link> : <div></div> }
+           {this.props.topic !== 'football' ? <Link to='/articles/topic/football' > <button>football</button> </Link>  : <div></div> }
+           {this.props.topic !== 'cooking' ? <Link to='/articles/topic/cooking' > <button>cooking</button> </Link> : <div></div> }
         </React.Fragment >
     )
 }
