26d9520bff3658d751bf8cd09225922205da3396
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 14:45:55 +0100

    receiving username as props & passing down to Comment

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 
 import Comment from './Comment';
 
-const CommentsList = ({comments, deleteComment}) =>  {
-
+const CommentsList = ({comments, deleteComment, username}) =>  {
         return (
             <div>
                 <h4>Comments</h4>
-                {comments.map(comment => <Comment key={comment._id} comment={comment} deleteComment={deleteComment}/>)}
+                {comments.map(comment => <Comment key={comment._id} comment={comment} deleteComment={deleteComment} username={username}/>)}
             </div>
         )
 }
