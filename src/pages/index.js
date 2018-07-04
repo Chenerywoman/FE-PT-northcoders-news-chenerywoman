2ee30aba746d0b5334889c14125f2ae0ed754877
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 20:27:09 +0100

    adding link to PostArticlePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import ArticlesPage from './ArticlesPage';
 import ArticlePage from './ArticlePage';
 import NoMatchPage from './NoMatchPage';
 import MainPage from './MainPage';
+import PostArticlePage from './PostArticlePage';
 
-export {HomePage, MainPage, ArticlesPage, ArticlePage, NoMatchPage};
+export {HomePage, MainPage, ArticlesPage, ArticlePage, NoMatchPage, PostArticlePage};
