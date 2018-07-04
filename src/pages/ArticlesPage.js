1113f582687ed20d33ef2e91ed90e6081a8ef087
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 14:12:49 +0100

    adding links to all articles & homepage to articles page

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { ArticleList, Navbar } from '../components'
@@ -65,6 +66,8 @@ class ArticlesPage extends Component {
                 {this.state.loading ? <div>Loading...</div>
                     :
                     <div>
+                        <Link to='/articles'><p>Return to all articles</p></Link>
+                        <Link to='/'><p>Return to HomePage</p></Link>
                         <button onClick={this.handleDownClick} disabled={this.state.page < 2 ? true : false}> Down one page...</button>
                         <button onClick={this.handleUpClick} disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button>
                         <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
