fa63397777680568206792f4d574b9f2578feb10
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 15:07:52 +0100

    refactoring delete comments to functional component from class & just a delete button, as id, deleteComment function & username passed as props

--- src/components/DeleteComment.js
+++ src/components/DeleteComment.js
@@ -1,37 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class DeleteComment extends Component  {
+const DeleteComment = ({deleteComment, id, username}) =>  {
 
-    state = {
-        userName: '',
-        err: ''
-    }
-
-    handleNameChange = event => {
-        this.setState({userName:event.target.value})
-      }
-
-handleClick = (event) => {
+const handleClick = (event) => {
     event.preventDefault();
-    if (this.props.name !== this.state.userName) { this.setState({err: 'only comment creator can delete comment'}) }
-    else {
-    this.props.deleteComment(this.props.id, this.state.userName)
-    this.setState({userName: ''})
-    }
+   deleteComment(id, username)
+  
 }
-
-    render() {
-        return <form onSubmit={this.handleClick}>
+        return (<form onSubmit={handleClick}>
         <h4>Delete comment</h4>
-        {this.state.err ? <div> {this.state.err}</div> : <div></div> }
-        <label> 
-        username: <input name='userName' type='text' placeholder='username here' onChange={this.handleNameChange} value={this.state.userName} /> 
-        </label>
         <button type="submit" >Delete</button>
         </form>
-    }
-    
-
+        )
 }
 
 export default DeleteComment;
