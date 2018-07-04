d3f866d433eb4dabf188d71005253bfa82237490
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-04 11:58:55 +0100

    adding beginning & end regex to index & page in state, adding if clause to componentDidMount

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -9,8 +9,8 @@ class MainPage extends Component {
   state = {
     articles: [],
     loading: true,
-    page: this.props.location.search ? /\d/.exec(this.props.location.search) : 1,
-    index: this.props.location.search ? /\d/.exec(this.props.location.search) - 1 : 0,
+    page: this.props.location.search ? /^\d+$/.exec(this.props.location.search) : 1,
+    index: this.props.location.search ? /^\d+$/.exec(this.props.location.search) - 1 : 0,
   }
 
   fetchArticles = () => {
@@ -41,7 +41,14 @@ class MainPage extends Component {
 
   componentDidMount() {
     this.fetchArticles()
+    .then(articles => {
+      console.log('this.state.articles.length', this.state.articles.length, 'this.state.page', this.state.index)
+      if (this.state.index > this.state.articles.length -1 || this.state.index < 0 || !/^\d+$/.test(this.state.index) ){
+      this.props.history.push('/404')
+    }
+  })
   }
+
 
   render() {
     return (
@@ -54,12 +61,11 @@ class MainPage extends Component {
             <button onClick={this.handleDownClick} disabled={this.state.page < 2 ? true : false}> Down one page...</button>
             <button onClick={this.handleUpClick} disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button>
             <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
-            <ArticleList topic='' articles={this.state.articles[this.state.index]} />
+            <ArticleList topic='' articles={this.state.articles[this.state.index]} history={this.props.history}/>
           </div>
         }
       </div>
     );
-
   }
 }
 
