16b776b3523ec0b7e31af63c394faf8f6ac08514
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-05 10:38:00 +0100

    add link to changing username login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import TopicChooser from './TopicChooser';
 
 const Navbar = ({username, topic}) => (<nav> 
     <h2>Logged in as {username}</h2> 
+    <Link to='/'>login as a different username</Link>
+
     <TopicChooser topic={topic}/>
 </nav>)
 
