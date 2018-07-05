c24dee3c72029c03cdfd07c9aa774d6ae377658f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-05 18:16:30 +0100

    formatting

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Article from './Article';
 
-const ArticleList = ({articles, topic}) => {
+const ArticleList = ({ articles, topic }) => {
     return (
         !articles ? <Redirect to='/404' /> :
-        <div>
-            <h2>Articles {topic ? <span> about {topic} </span> : '' } </h2>
-            {articles.map(article => 
-             <Article key={article._id} article={article} />
-            )}
-        </div>
+            <div>
+                <h2>Articles {topic ? <span> about {topic} </span> : ''} </h2>
+                {articles.map(article =>
+                    <Article key={article._id} article={article} />
+                )}
+            </div>
     )
 }
 
