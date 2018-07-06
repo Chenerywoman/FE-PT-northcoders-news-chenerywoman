5cc5b8c6e354797236b394383f5b1e44ff5da4c8
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-06 21:24:10 +0100

    adding link to articles as well as homepage

--- src/pages/NoMatchPage.js
+++ src/pages/NoMatchPage.js
@@ -3,11 +3,15 @@ import {Link} from 'react-router-dom';
 
 const NoMatchPage = props => {
     return (
+      <React.Fragment>
       <div>
         <h2> 404: not found. </h2>
-        <Link to='/'> Go back home---> </Link>
+        <div><Link to='/'> Homepage---> </Link></div>
+        <p></p>
+        <Link to='/articles'> All articles---> </Link>
   
       </div>
+      </React.Fragment>
     )
   }
 
