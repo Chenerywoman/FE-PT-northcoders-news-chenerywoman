c3c223bd48d28aa9a05b782ef9edcae0726000c4
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-06 18:57:05 +0100

    refactoring Navbar using <li> & restyling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../styling/components/Navbar.css'
 
-import TopicChooser from './TopicChooser';
-
-const Navbar = ({username, topic}) => (<nav> 
-    <h2>Logged in as {username}</h2> 
-    <Link to='/'>login as a different username</Link>
-
-    <TopicChooser topic={topic}/>
-</nav>)
+const Navbar = ({ username, topic, page }) => (
+    <nav >
+        <ul id='list'>
+            <li id='logo' className='link' >  NCnewsLogo</li>
+            <li id='home' ><Link className='link' to='/'>Home</Link></li>
+            <li id='articles'><Link className='link'  to='/articles'>All</Link></li>
+            <li id='coding'><Link className='link'  to='/articles/topic/coding' >Coding</Link></li>
+            <li id='football'><Link className='link'  to='/articles/topic/football' >Cooking</Link></li>
+            <li id='cooking'><Link className='link'  to='/articles/topic/cooking' >Football</Link></li>
+            <li id='post'><Link className='link' to='articles/postarticle'>Post</Link></li>
+            <li id='login'><Link className='link'  to='/'>{`${username}`}</Link></li>    
+        </ul>
+    </nav>
+)
 
 Navbar.PropTypes = {
 
