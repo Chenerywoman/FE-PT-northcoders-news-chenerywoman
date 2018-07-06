3b59e71c3c29daee0f2683874e38dcbec6516a37
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-06 21:18:39 +0100

    function to check for an empty object

--- src/dataFunctions/helpers.js
+++ src/dataFunctions/helpers.js
@@ -34,3 +34,11 @@ export const chunkArray = (array, chunk) => {
     
     return results;
 }
+
+export const isEmpty = (obj) => {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
