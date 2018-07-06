f7613e9d8941d68e0b231229715b1010cbbfb939
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-06 21:40:03 +0100

    styling NoMatch page

--- src/pages/NoMatchPage.js
+++ src/pages/NoMatchPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../styling/pages/NoMatch.css'
 
 const NoMatchPage = props => {
     return (
       <React.Fragment>
       <div>
         <h2> 404: not found. </h2>
-        <div><Link to='/'> Homepage---> </Link></div>
+        <div><Link className='link-back' to='/'> Homepage---> </Link></div>
         <p></p>
-        <Link to='/articles'> All articles---> </Link>
+        <Link className='link-back' to='/articles'> All articles---> </Link>
   
       </div>
       </React.Fragment>
