b250fd702d96ae014859e569d6ccdce9fec791d7
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-06 21:23:09 +0100

    removing empty topic prop passed to Navbar & removing links now in Navbar

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../styling/pages/MainPage.css'
 
 import { ArticleList, Navbar } from '../components'
 import { fetchAllArticles } from '../dataFunctions/api';
@@ -49,11 +50,10 @@ class MainPage extends Component {
     return (
       <div>
 
-        <Navbar username={this.props.username} />
+        <Navbar username={this.props.username} page='main'/>
         {this.state.loading ? <div>Loading...</div>
           :
           <div>
-             <Link to='/'><p>Return to HomePage</p></Link>
             <button onClick={this.handleDownClick} disabled={this.state.page < 2 ? true : false}> Down one page...</button>
             <button onClick={this.handleUpClick} disabled={this.state.page > this.state.articles.length - 1 ? true : false} > Up one page...</button>
             <p>Page {`${this.state.page}`} of {`${this.state.articles.length}`}</p>
