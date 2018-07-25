e847224d7c2d095727d84e871617adc7bad4b97a
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-25 19:18:23 +0100

    refactoring to use ArticlesPage for /articles route as well as /articles/topic/:topic

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ class App extends Component {
                 <React.Fragment>
                 <Navbar className='navbar' username={this.state.username}/>
                     <Switch>
-                        <Route exact path='/' render={props => (< HomePage logUser={this.logUser} username={this.state.username}/> )} />
-                        <Route exact path='/articles' render={props => (<MainPage username={this.state.username} {...props} /> )} /> 
-                        <Route exact path='/articles/topic/:topic' render={props => <ArticlesPage username={this.state.username} {...props} />} />
-                        <Route exact path='/articles/:id' render={props => <ArticlePage username={this.state.username}{...props}/>} />
-                        <Route exact path='/postarticle' render={props => <PostArticlePage username={this.state.username} {...props} />} />
+                        <Route exact path='/' render={props => < HomePage logUser={this.logUser} username={this.state.username}/> } />
+                        <Route exact path='/articles' render={props => <ArticlesPage {...props} /> } /> 
+                        <Route exact path='/articles/topic/:topic' render={props => <ArticlesPage {...props} />} />
+                        <Route exact path='/articles/:id' render={props => <ArticlePage {...props}/>} />
+                        <Route exact path='/postarticle' render={props => <PostArticlePage username={this.state.username}{...props} />} />
                         <Route component={NoMatchPage} />
                     </Switch>
                 </React.Fragment>
