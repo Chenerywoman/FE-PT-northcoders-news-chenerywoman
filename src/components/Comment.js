f6abdd55fe8a82bc209e195105bdf5c1924aa3c2
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-26 07:27:20 +0100

    changing ternary with empty div to condition && div to display

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,15 +5,15 @@ import Votes from './Votes';
 import DeleteComment from './DeleteComment';
 
 const Comment = ({ comment, deleteComment, username }) => {
+    console.log('comment.created_by.username', comment.created_by.username, 'username', username)
     return (
         <div>
             <p> date:{comment.created_at} </p>
             <p>{comment.body}</p>
             <Votes votes={comment.votes} id={comment._id} route='comments' />
             <User name={comment.created_by.username} avatar={comment.created_by.avatar_url} />
-            { username === comment.created_by.username ?
+            { username === comment.created_by.username &&
             <DeleteComment name={comment.created_by.username} id={comment._id} deleteComment={deleteComment} username={username}/>
-            : <div></div>
             }
         </div>
     )
