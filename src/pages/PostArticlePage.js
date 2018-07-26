49d22bbdeb31d1d8540255c16109d78cad303bab
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-26 07:38:14 +0100

    passing username as props to CommentList

--- src/pages/PostArticlePage.js
+++ src/pages/PostArticlePage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import {ArticleBox, Navbar} from '../components';
+import {ArticleBox} from '../components';
 import {postArticleText} from '../dataFunctions/api';
 
 
