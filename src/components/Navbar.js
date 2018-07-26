a577ac14efb08ed419cab1cd9d3417113be431e7
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-26 20:24:47 +0100

    adding classes to Navbar for styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,15 @@ class Navbar extends Component {
     render(){
     return (
     <nav>
-        <ul className='flex-container-navbar'  >
-            <li id='logo' className='link'> NCnewsLogo</li>
-            <li ><NavLink activeClassName='link-active' className='link' exact to='/'>Home</NavLink></li>
-            <li ><NavLink activeClassName='link-active' className='link' exact to='/articles'>All</NavLink></li>
-            <li ><NavLink activeClassName='link-active' className='link' to='/articles/topic/coding' >Coding</NavLink></li>
-            <li ><NavLink activeClassName='link-active' className='link' to='/articles/topic/football' >Football</NavLink></li>
-            <li ><NavLink activeClassName='link-active' className='link' to='/articles/topic/cooking' >Cooking</NavLink></li>
-            <li ><NavLink activeClassName='link-active' className='link' to='/postarticle'>Post</NavLink></li>
-            <li> <NavLink id='login' className='link' to='/'>{`${this.props.username}`}</NavLink></li>    
+        <ul className='navbar'  >
+            <li id='logo' className='link nav-link'> NCnewsLogo</li>
+            <li className='nav-link'><NavLink activeClassName='link-active' className='link' exact to='/'>Home</NavLink></li>
+            <li className='nav-link'><NavLink activeClassName='link-active' className='link' exact to='/articles'>All</NavLink></li>
+            <li className='nav-link'><NavLink activeClassName='link-active' className='link' to='/articles/topic/coding' >Coding</NavLink></li>
+            <li className='nav-link'><NavLink activeClassName='link-active' className='link' to='/articles/topic/football' >Football</NavLink></li>
+            <li className='nav-link'><NavLink activeClassName='link-active' className='link' to='/articles/topic/cooking' >Cooking</NavLink></li>
+            <li className='nav-link'><NavLink activeClassName='link-active' className='link' to='/postarticle'>Post</NavLink></li>
+            <li className='nav-link'> <NavLink id='login' className='link' to='/'>{`${this.props.username}`}</NavLink></li>    
         </ul>
     </nav>
     )
