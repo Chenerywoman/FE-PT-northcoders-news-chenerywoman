039e07fab11b495cdc61a60ba5e20a0f8531a34a
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-27 13:37:48 +0100

    changing reference importing user.css - capital letter

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../styling/components/user.css';
+import '../styling/components/User.css';
 
 const User = ({name, avatar}) => {
 
