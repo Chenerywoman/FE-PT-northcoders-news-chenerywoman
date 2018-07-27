a96d031e9aebe1035102c434d1344a89b063d1db
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-27 07:54:03 +0100

    changing layout for styling

--- src/components/User.js
+++ src/components/User.js
@@ -5,10 +5,10 @@ import '../styling/components/user.css';
 const User = ({name, avatar}) => {
 
 return (
-    <div>
-  <p>  created by: {name}</p>
+    <span>
+  <p>{name}</p>
     <img id='avatar' src={avatar} alt="avatar" height='75' width='75'></img>
-    </div>
+    </span>
 )
 }
 
