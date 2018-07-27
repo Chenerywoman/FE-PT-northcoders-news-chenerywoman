66e6eeb3dd8cbaa6b8a8e7e014f61c02a9e36b9d
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-27 07:08:24 +0100

    styling User

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styling/components/user.css';
 
 const User = ({name, avatar}) => {
 
 return (
     <div>
- <p>{name}</p>
-    <img src={avatar} alt="avatar"></img>
+  <p>  created by: {name}</p>
+    <img id='avatar' src={avatar} alt="avatar" height='75' width='75'></img>
     </div>
 )
 }
