a5c711c2e3845077bd2060691dd853944a5c856e
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-30 17:25:44 +0100

    amending inline style text-decoration to React textDecoration

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,10 +9,10 @@ const Article = ({article}) => {
 
     return (
         <div id='article-container'>
-          <div id='article-title'> <Link style={{'text-decoration': 'none'}} key={article._id} to={`/articles/${article._id}`} >  <p id='article-title-wording' >{article.title}</p> </Link></div>
+          <div id='article-title'> <Link style={{'textDecoration': 'none'}} key={article._id} to={`/articles/${article._id}`} >  <p id='article-title-wording' >{article.title}</p> </Link></div>
           <div id='user'><User name={article.created_by.username} avatar={article.created_by.avatar_url} /></div>
             <div id='topic' >topic: {article.belongs_to.title}</div>
-            <p id='article'><Link style={{'text-decoration': 'none'}} key={article._id} to={`/articles/${article._id}`} >{article.body}</Link></p>
+            <p id='article'><Link style={{'textDecoration': 'none'}} key={article._id} to={`/articles/${article._id}`} >{article.body}</Link></p>
           <div id='comments'>comments: {article.comments}</div>
           <div id='votes'><Votes votes={article.votes} id={article._id} route="articles"/></div>
          </div>
