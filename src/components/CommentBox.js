0d39331f0a3c89648036ce831c3454b0559757c9
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-30 23:02:45 +0100

    styling CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,35 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styling/components/CommentBox.css'
 
-class CommentBox extends Component { 
+class CommentBox extends Component {
 
-state = {
-    text: '',
-    err: ''
-}
+    state = {
+        text: '',
+        err: ''
+    }
 
-handleTextChange = event => {
-  this.setState({text:event.target.value})
-}
+    handleTextChange = event => {
+        this.setState({ text: event.target.value })
+    }
 
-handleClick = event => {
-    event.preventDefault();
-    this.props.postComment(this.props.username, this.state.text)
-    .then(res => this.setState({userName:'', text: ''}))
-    .catch(err => this.setState({err: err.statusText}))
-}
+    handleClick = event => {
+        event.preventDefault();
+        this.props.postComment(this.props.username, this.state.text)
+            .then(res => this.setState({ userName: '', text: '' }))
+            .catch(err => this.setState({ err: err.statusText }))
+    }
 
-render(){
-return (<form onSubmit={this.handleClick}>
-<h5>Add a comment</h5>
-{this.state.err &&  <div> {this.state.err} </div> }
-    <label> Add text:
-    <textarea name="comment" id="textbox" cols="52" rows="5" onChange={this.handleTextChange} value={this.state.text} placeholder='add text' /> 
-    </label>
-    <button type="submit" disabled={!this.state.text} >Submit</button>
-    </form>
-)
-}
+    render() {
+        return (
+
+            <form id='comment-box' onSubmit={this.handleClick}>
+                <div id='comment-box-title'>Add a comment</div>
+                {this.state.err && <div> {this.state.err} </div>}
+                    <textarea name="comment" id="textbox" cols="52" rows="5" onChange={this.handleTextChange} value={this.state.text} placeholder='write your comment here...' />
+                <button id='comment-box-submit' type="submit" disabled={!this.state.text} >Submit</button>
+            </form>
+        )
+    }
 }
 
 CommentBox.propTypes = {
