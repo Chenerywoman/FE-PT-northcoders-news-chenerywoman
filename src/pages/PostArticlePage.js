a3fd1d956102d796a554d01e7adcc295d596abc2
Chenerywoman <rachelmchenery@hotmail.com>
2018-08-01 19:13:51 +0100

    styling postedArticle page

--- src/pages/PostArticlePage.js
+++ src/pages/PostArticlePage.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-
 import {ArticleBox} from '../components';
 import {postArticleText} from '../dataFunctions/api';
+import '../styling/pages/PostArticlePage.css'
 
 
 class PostArticlePage extends Component {
 
     state = {
         new_article: {},
+        posted: false,
     }
 
     postArticle = (title, article, topic) => {
@@ -19,23 +20,23 @@ class PostArticlePage extends Component {
             if (res.error) {
                 return res.error
             } else { 
-                this.setState({new_article: res.new_article}) 
+                this.setState({new_article: res.new_article, posted: true}) 
                 return res.new_article
             }
         }
         )   
 }
 render() {
-    return (
+    return !this.state.posted ?
+    <ArticleBox id='post-article-box' postArticle={this.postArticle}/> 
+    :
     <React.Fragment>
-    <ArticleBox postArticle={this.postArticle}/>
-    {
-    this.state.new_article && 
-     <Link to={`/articles/${this.state.new_article._id}`} > <span>{this.state.new_article.title}</span> </Link>
-    }
+    <p id='posted-success-title'>Your posted article:</p>
+     <Link id='posted-article-link' to={`/articles/${this.state.new_article._id}`} > <span id='posted-article'>{this.state.new_article.title}</span> </Link>
     </React.Fragment>
-    )
-}
+    }
+
+
 }
 
 
