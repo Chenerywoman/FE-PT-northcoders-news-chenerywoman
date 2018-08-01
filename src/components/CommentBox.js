ac9063ce81bde77540b4f197aea09f39c7d810e1
Chenerywoman <rachelmchenery@hotmail.com>
2018-08-01 17:01:57 +0100

    adding avatar to title

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import '../styling/components/CommentBox.css'
+import '../styling/components/CommentBox.css';
+import {getUser} from '../dataFunctions/api';
 
 class CommentBox extends Component {
 
     state = {
         text: '',
-        err: ''
+        err: '',
+        avatar: '',
     }
 
     handleTextChange = event => {
@@ -20,11 +22,33 @@ class CommentBox extends Component {
             .catch(err => this.setState({ err: err.statusText }))
     }
 
+    componentDidMount () {
+        if (this.props.username) {
+        getUser(this.props.username)
+        .then(({user}) => {
+            console.log('user in didMount', user)
+            this.setState({avatar: user.avatar_url})
+
+        })
+    }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.username !== prevProps.username){
+        getUser(this.props.username)
+        .then(({user})=> {
+            console.log('user in didUpdate', user)
+            this.setState({avatar: user.avatar_url})
+
+        })
+    }
+    }
+
     render() {
         return (
 
             <form id='comment-box' onSubmit={this.handleClick}>
-                <div id='comment-box-title'>Add a comment</div>
+                <div id='comment-box-title'><span>Add a comment    </span><img id='comment-avatar' src={this.props.username === 'cooljmessy' ? "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/185?cb=20170730171002" : this.props.username === 'weegembump' ? "https://vignette.wikia.nocookie.net/mrmen/images/f/fb/MR_BUMP_3A.PNG/revision/latest/scale-to-width-down/150?cb=20170527174939" : this.state.avatar} alt="avatar" height='35' width='35'></img></div>
                 {this.state.err && <div> {this.state.err} </div>}
                     <textarea name="comment" id="textbox" cols="52" rows="5" onChange={this.handleTextChange} value={this.state.text} placeholder='write your comment here...' />
                 <button id='comment-box-submit' type="submit" disabled={!this.state.text} >Submit</button>
